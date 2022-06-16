f7174bddc87ed41d67fa95451dc8e35f2e0285dd
TANNER MECK <steve.meck246@gmail.com>
2022-06-16 12:57:21 -0700

    all tests passign

--- exercise_14/exercise_14.sql
+++ exercise_14/exercise_14.sql
@@ -1,2 +1,7 @@
 -- the average film length by category
 -- include the category name and avg length
+SELECT avg(film.length) as avg, category.name
+FROM category
+LEFT JOIN film_category ON film_category.category_id = category.category_id
+LEFT JOIN film ON film.film_id = film_category.film_id
+GROUP BY category.name
